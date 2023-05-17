23e4b0c277fc763559f53daff682bd4b809379aa
Qolbyltfh <qolbi07latifah@gmail.com>
2023-05-17 22:43:20 +0700

    fixing authentication role

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -19,5 +19,6 @@ class IsAdmin
             return $next($request);
         }
    
+        return redirect('home')->with('error',"You don't have admin access.");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
